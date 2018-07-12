f41f05c9732e8962da28edb3a25bae651d811992
Eva van Rooden <evavanrooden@gmail.com>
2018-07-12 09:34:40 +0200

    added Airport and Airplane class

--- src/main/java/com/XYZAirline/exercise/App.java
+++ src/main/java/com/XYZAirline/exercise/App.java
@@ -1,13 +1,17 @@
 package com.XYZAirline.exercise;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+// @ComponentScan(basePackages = "com.hotelmolveno") // vragen aan Raymond
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
---
+++ src/main/java/com/XYZAirline/exercise/model/Airplane.java
@@ -0,0 +1,23 @@
+package com.XYZAirline.exercise.model;
+
+public class Airplane {
+
+    private long id;
+    private int tonsOfFuel;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getTonsOfFuel() {
+        return tonsOfFuel;
+    }
+
+    public void setTonsOfFuel(int tonsOfFuel) {
+        this.tonsOfFuel = tonsOfFuel;
+    }
+}
